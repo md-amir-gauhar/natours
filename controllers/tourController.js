c593159f668757b09468292a0294e0ab853fed46
md-amir-gauhar <gauharamir15@gmail.com>
2021-12-07 11:00:35 +0530

    completed the CRUD operations

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -108,11 +108,19 @@ const updateTour = async (req, res) => {
   }
 }
 
-const deleteTour = (req, res) => {
-  res.status(204).json({
-    status: "success",
-    data: null
-  })
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id)
+    res.status(204).json({
+      status: "success",
+      data: null
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err
+    })
+  }
 }
 
 module.exports = { getAllTours, createTour, getTour, updateTour, deleteTour };
